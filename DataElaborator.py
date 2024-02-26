ada6b8b2f80499c44501a6310e1a898aa51d5016
Abudo-S <abudo.mohamed123456789@gmail.com>
2024-02-26 20:18:49 +0100

    Implement timerEvaluationDecorator in DataElaborator. Implement extendCustomers and getFraudulentTransPerPeriod in DataElaborator

--- DataElaborator.py
+++ DataElaborator.py
@@ -2,11 +2,12 @@ from neo4j import GraphDatabase
 import datetime
 from enum import Enum
 import random
+import time
 
 # class TransactionPeriod (Enum):
 #     morning = 'morning', 
 #     afternoon = 'afternoon', 
-#     evening = 'afternoon', 
+#     evening = 'evening', 
 #     night = 'night'
 
 # class TransactionProductType (Enum):
@@ -15,6 +16,19 @@ import random
 #     clothing = 'clothing', 
 #     consumable = 'consumable', 
 #     other = 'other'
+
+def timerEvaluationDecorator(func):
+    def wrapper(*args):
+        start_time = time.time()
+        result = func(*args)
+        end_time = time.time()
+        execution_time = end_time - start_time
+
+        print(f"Function '{func.__name__}' took {execution_time:.4f} seconds!")
+
+        return result
+    
+    return wrapper
 
 class DataElaborator():
     
@@ -39,6 +53,7 @@ class DataElaborator():
     return c.CustomerID, spendingFrequency, spendingAmount, 
     CASE WHEN spendingFrequency < c.spending_frequency  and spendingAmount < c.spending_amount THEN true ELSE false END AS underTheLimit
     '''
+    @timerEvaluationDecorator
     def getCustomerLastMonthLimit(self):
         query = "MATCH "
         query += f"(c:Customer)-[:Invoke]-(t:Transaction) where t.datetime > '{str(datetime.datetime.now().date() - datetime.timedelta(days=31))}' "\
@@ -53,13 +68,16 @@ class DataElaborator():
     match (t:Terminal) -[:On]- (tx:Transaction) where tx.datetime > '2024-01-26'
     with t, 0.2 * max(tx.amount) as threshold, collect(tx.TransID) as transactionIds
     match (tx:Transaction) where tx.TransID in transactionIds and tx.amount > threshold
+    set tx.isFraudulent = true
     return t.TermID, tx.amount, threshold
     '''
+    @timerEvaluationDecorator
     def getFraudulantTransactionsPerTerminal(self):
         query = "MATCH "
         query += f"(t:Terminal) -[:On]- (tx:Transaction) where tx.datetime > '{str(datetime.datetime.now().date() - datetime.timedelta(days=31))}' "\
                 f"with t, 0.2 * max(tx.amount) as threshold, collect(tx.TransID) as transactionIds "\
                 f"Match (tx:Transaction) where tx.TransID in transactionIds and tx.amount > threshold "\
+                f"Set tx.isFraudulent = true "\
                 f"return t.TermID, tx.amount, threshold"
         
         return self.run(query)
@@ -69,6 +87,7 @@ class DataElaborator():
 
         #match (c1:Customer) -[:Use*1..3]- (c2:Customer) return c1, c2
     '''
+    @timerEvaluationDecorator
     def getCocustomerOfCustomer(self, customerId:int, degree:int = 3):
         query = "MATCH "
         query += f"(c:Customer {{id: {customerId}}}) -[:Use*{degree - 1}..{degree - 1}]-(:Terminal)-[:Use]-(c1:Customer) "\
@@ -93,9 +112,10 @@ class DataElaborator():
             when 3 then 'consumable'
             else 'other'
         end
-    set t.feeling=  round(rand()*4)
+    set t.feeling=  round((rand()*4) +1)
     #Rand() restituisce un numero frazionale in [0:1] 
     '''
+    @timerEvaluationDecorator
     def extendTransactions(self):
         query = "Match "
         query += f"(t:Transaction) set t.period = "\
@@ -113,18 +133,49 @@ class DataElaborator():
                 f"when 3 then 'consumable' "\
                 f"else 'other' "\
                 f"end "\
-                f"set t.feeling = round(rand()*4)"
+                f"set t.feeling = round((rand()*4) + 1)"
         
         return self.run(query)
 
     '''pt.d2
+    match (c:Customer)-[:Invoke]-(tx:Transaction)-[:On]-(t:Terminal)-[:On]-(tx1:Transaction)-[:Invoke]-(c1:Customer) 
+    with c, c1, t, collect(tx) as cTx, collect(tx1) as c1Tx1, avg(tx.feeling) as avgC, avg(tx1.feeling) as avgC1 
+    where size(cTx) > 3 and size(c1Tx1) > 3 and abs(avgC - avgC1) < 1 and c <> c1 
+    merge (c)-[:Buying_friends]->(c1)
     '''
+    @timerEvaluationDecorator
     def extendCustomers(self):
-        return
+        query = "Match "
+        query += f"(c:Customer)-[:Invoke]-(tx:Transaction)-[:On]-(t:Terminal)-[:On]-(tx1:Transaction)-[:Invoke]-(c1:Customer) "\
+                f"with c, c1, t, collect(tx) as cTx, collect(tx1) as c1Tx1, avg(tx.feeling) as avgC, avg(tx1.feeling) as avgC1 "\
+                f"where size(cTx) > 3 and size(c1Tx1) > 3 and abs(avgC - avgC1) < 1 and c <> c1 "\
+                f"merge (c)-[:Buying_friends]->(c1)"
+        
+        return self.run(query)
+    
+    '''pt.e
+    match (trans:Transaction) with trans.period as period, count(trans) as numPeriodTrans, collect(trans.TransID) as periodTransIds
+    match (t:Transaction) where t.TransID in periodTransIds and t.isFraudulent = true 
+    return period, numPeriodTrans, toFloat(count(t))/toFloat(numPeriodTrans) as avgFraudulentTrans
+    '''
+    @timerEvaluationDecorator
+    def getFraudulentTransPerPeriod(self, executeFraudulantTransMarker = False):
+
+        if executeFraudulantTransMarker:
+            #prepara le transactions fraudulente supponendo che getFraudulantTransactionsPerTerminal() non è stato chiamato:
+            print(self.getFraudulantTransactionsPerTerminal())
+
+        query = "Match "
+        query += f"(trans:Transaction) with trans.period as period, count(trans) as numPeriodTrans, collect(trans.TransID) as periodTransIds "\
+                f"match (t:Transaction) where t.TransID in periodTransIds and t.isFraudulent = true  "\
+                f"return period, numPeriodTrans, toFloat(count(t))/toFloat(numPeriodTrans) as avgFraudulentTrans"
+        
+        return self.run(query)
     
     '''
     match (n) detach delete n
     '''
+    
     def resetDB(self):
         query = "MATCH "
         query += f"(n) detach delete n"
